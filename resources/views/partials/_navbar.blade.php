ab7b40f16ff4e4e57629d80d2dec78edf56bbba0
hanip303 <hanipalhapidz56@gmail.com>
2024-01-29 17:58:21 +0700

    Add mobile menu dropdown

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -7,7 +7,7 @@
         </a>
 
         @auth
-            <div class="flex items-center gap-4">
+            <div class="hidden sm:flex items-center gap-4">
                 <a href="/posts/manage" class="transition hover:text-blue-500 cursor-pointer flex items-center gap-1">
                     <i class="fa-solid fa-gear"></i> posts
                 </a>
@@ -17,7 +17,7 @@
             </div>
         @endauth
 
-        <div class="flex items-center gap-4">
+        <div class="hidden sm:flex items-center gap-4">
             @auth
                 <form action="/logout" method="POST">
                     @csrf
@@ -40,5 +40,45 @@
                 </a>
             @endauth
         </div>
+
+        {{-- mobile menu bar --}}
+        <div class="block sm:hidden relative" x-data="{ open: false }">
+            <button x-on:click="open=!open" id="open-menu-mobile" class="text-lg">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+
+            {{-- menu --}}
+            <div class="absolute top-7 right-0 bg-white shadow-md w-40 flex flex-col" x-show="open"
+                @click.outside="open = false">
+                @auth
+                    <a href="/posts/manage"
+                        class="transition hover:bg-gray-100 cursor-pointer flex items-center gap-2 py-2 px-4">
+                        <i class="fa-solid fa-gear"></i> posts
+                    </a>
+                    <a href="/posts/create"
+                        class="transition hover:bg-gray-100 cursor-pointer flex items-center gap-2 py-2 px-4">
+                        <i class="fa-solid fa-plus"></i> Create
+                    </a>
+                    <form action="/logout" method="POST">
+                        @csrf
+                        <button type="submit"
+                            class="transition hover:bg-gray-100 cursor-pointer flex items-center gap-2 py-2 px-4 w-full border-t">
+                            <i class="fa-solid fa-right-to-bracket"></i> Logout
+                        </button>
+                    </form>
+                @else
+                    <a href="/register"
+                        class="transition hover:bg-gray-100 cursor-pointer flex items-center gap-2 py-2 px-4">
+                        <i class="fa-solid fa-address-card"></i> Register
+                    </a>
+                    <a href="/login" class="transition hover:bg-gray-100 cursor-pointer flex items-center gap-2 py-2 px-4">
+                        <i class="fa-solid fa-right-to-bracket"></i> Login
+                    </a>
+                @endauth
+
+            </div>
+        </div>
+
+
     </x-container>
 </div>
